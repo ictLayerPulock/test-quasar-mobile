dcb603c1555e91c9db241aaae52f288de0a82f28
ictLayerPulock <157199030+ictLayerPulock@users.noreply.github.com>
2025-02-16 17:24:25 +0600

    Update showroom

--- pages/showroom/index.vue
+++ pages/showroom/index.vue
@@ -11,38 +11,60 @@
         </h1>
       </q-card-section>
       <q-separator />
-      <!-- {{ response.showroomList }} -->
       <q-card-section class="q-pa-none">
-        <q-card v-for="location in response.showroomList" :key="location" class="q-ma-sm" style="height: 150px">
-          <q-card-section horizontal class="row overflow-hidden cursor-pointer"
+        <q-card v-for="location in response.showroomList" :key="location" class="q-ma-sm" style="height: 120px">
+          <q-card-section horizontal>
+              <NuxtImg loading="lazy" @click="navigateTo(`/showroom/${location.slug}`)" rounded height="120" src="https://placehold.co/200x200" :title="location.name"
+                :alt="location.name" />
+            <div class="q-pa-xs">
+              <div class="row items-center q-col-gutter-x-sm" @click="navigateTo(`/showroom/${location.slug}`)">
+                <q-icon size="xs" color="red" name="place" />
+                <p class="q-ma-none text-subtitle2"> {{ location.name }}</p>
+              </div>
+              <div class="row items-center q-col-gutter-x-sm">
+                <q-icon size="xs" color="red" name="home" />
+                <p class="q-ma-none text-caption  ellipsis-2-lines" @click="navigateTo(`/showroom/${location.slug}`)"> {{ location.address }}</p>
+              
+              </div>
+              <div class="row items-center q-col-gutter-x-sm">
+                <q-icon size="xs" color="red" name="phone" />
+                <p class="q-ma-none text-caption"> 
+                  <a :href="`tel:` + location.phone" style="text-decoration: none" class="text-secondary">
+                      {{ location.phone }}
+                    </a>
+                </p>
+              </div>
+            </div>
+          </q-card-section>
+          <!-- <q-card-section horizontal class="row overflow-hidden cursor-pointer"
             @click="navigateTo(`/showroom/${location.slug}`)">
             <div class="col-4"> 
-              <NuxtImg loading="lazy" rounded height="170" class="fit" :src="location.image" :title="location.name"
+              <NuxtImg loading="lazy" rounded ratio="3:4" height="150" width="112" :src="location.image" :title="location.name"
                 :alt="location.name" />
             </div>
             <q-card-section class="q-pa-none column justify-center">
-              <q-list class="q-gutter-y-sm" padding>
+              <q-list class="q-gutter-y-sm">
                 <q-item dense>
                   <q-item-section side>
-                    <q-icon size="sm" color="red" name="place" />
+                    <q-icon size="xs" color="red" name="place" />
                   </q-item-section>
-                  <q-item-section class="text-body1 text-weight-medium">
+                  <q-item-section class="text-body2 text-weight-medium">
                     {{ location.name }}
                   </q-item-section>
                 </q-item>
                 <q-item dense>
                   <q-item-section side>
-                    <q-icon size="sm" color="primary" name="home" />
+                    <q-icon size="xs" color="primary" name="home" />
                   </q-item-section>
-                  <q-item-section class="text-body2 ellipsis-2-lines">
+                  <q-item-section class="text-caption ellipsis-2-lines">
                     {{ location.address }}
                   </q-item-section>
                 </q-item>
                 <q-item dense>
                   <q-item-section side>
-                    <q-icon size="sm" color="primary" name="phone" />
+                    <q-icon size="xs" color="primary" name="phone" />
                   </q-item-section>
-                  <q-item-section class="text-body1">
+                  <q-item-section class="text-caption">
                     <a :href="`tel:` + location.phone" style="text-decoration: none" class="text-secondary">
                       {{ location.phone }}
                     </a>
@@ -50,7 +72,7 @@
                 </q-item>
               </q-list>
             </q-card-section>
-          </q-card-section>
+          </q-card-section> -->
         </q-card>
       </q-card-section>
     </q-card>
