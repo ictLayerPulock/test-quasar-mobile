101780484235f04e563945c2629d9a6848c410c4
ICT Layer <32593898+ICTLAYER@users.noreply.github.com>
2025-02-16 20:44:20 +0600

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["nuxt-quasar-ui", "@nuxt/image"],
+  modules: ["nuxt-quasar-ui", "@nuxt/image", "@nuxtjs/html-validator"],
   quasar: {
     extras: {
       fontIcons: ["material-icons"], // Add any icons you need
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
   build: {
     transpile: ["quasar"]
   },
-   
+
   runtimeConfig: {
     apiSecret: process.env.NUXT_API_SECRET,
     apiUrl: process.env.NUXT_API_URL,
